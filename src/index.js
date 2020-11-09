f34461c55aebd207ce9c7a33709021561720e896
Brian Chirls <brian@technologyhumansandtaste.com>
2020-11-09 09:28:55 -0500

    export Pointer in main script

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ export { default as Action} from './Action';
 // devices
 export { default as Gamepad } from './devices/gamepad';
 export { default as Keyboard } from './devices/keyboard';
+export { default as Pointer } from './devices/pointer';
 
 // controls
 export { default as InputControl } from './controls/InputControl';
