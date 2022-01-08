bbd853a81922bf6d604544645ace4e6c08cb9bd3
Brian Chirls <brian@chirls.com>
2022-01-08 15:38:15 -0500

    Document Device options and properties

--- src/devices/Device.ts
+++ src/devices/Device.ts
@@ -1,20 +1,53 @@
 import { InputControlBase } from '../controls/InputControl';
 
 export interface DeviceOptions {
+	/**
+	 * Determines initial value for `.enabled`, whether devices is initially
+	 * enabled upon creation.
+	 *
+	 * default: true
+	 */
 	enabled?: boolean;
 }
 
 export interface Device {
+	/**
+	 * Create a new InputControl object attached to the device.
+	 */
 	getControl: (name: string, options?: any) => InputControlBase;
+
+	/**
+	 * Remove any event listeners and free up any resources.
+	 */
 	destroy: () => void;
+
+	/**
+	 * Devices that are not enabled will not emit any events, and read operations
+	 * will always return the default value. Typically 0.
+	 */
 	enabled: boolean;
+
+	/**
+	 * Whether a devices is currently connected.
+	 */
 	readonly connected: boolean;
 }
 
 export interface PollingDeviceOptions extends DeviceOptions {
+	/**
+	 * For devices that poll hardware, `updatePeriod` determines the minimal
+	 * length of time between samples. Shorter periods (and therefore higher frequency)
+	 * might improve responsiveness but may impact performance and limit accuracy
+	 * of "delta" values between samples.
+	 */
 	updatePeriod?: number;
 }
 
 export interface PollingDevice extends Device {
+	/**
+	 * The time (DOMHighResTimeStamp) of the last input read from the device hardware,
+	 * in milliseconds, representing time since "time origin," or the beginning of
+	 * the document's lifetime.
+	 */
 	readonly timestamp: number;
 }
