627cd5f2e4f4bb1e1065c38d44dc13785f2c6a59
Brian Chirls <brian@chirls.com>
2022-04-10 21:50:30 -0400

    Replace toBeRoughly with toBeDeepCloseTo
    Does about the same thing, but this one is much easier to get Typescript configured

--- src/processors/axisDeadZone.test.ts
+++ src/processors/axisDeadZone.test.ts
@@ -1,11 +1,11 @@
 import axisDeadZone from './axisDeadZone';
 
 describe('axisDeadZone', () => {
-	it('middle value', () => expect(axisDeadZone(0.5, 0.1, 0.9)).toBeRoughly(0.5));
+	it('middle value', () => expect(axisDeadZone(0.5, 0.1, 0.9)).toBeCloseTo(0.5, 5));
 	it('low value', () => expect(axisDeadZone(0.1, 0.2, 0.9)).toBe(0));
 	it('high value', () => expect(axisDeadZone(0.9, 0.1, 0.8)).toBe(1));
-	it('interpolated value', () => expect(axisDeadZone(0.8, 0.2, 1)).toBeRoughly(0.75));
+	it('interpolated value', () => expect(axisDeadZone(0.8, 0.2, 1)).toBeCloseTo(0.75, 5));
 
-	it('should work without min and max', () => expect(axisDeadZone(0.525)).toBeRoughly(0.5));
+	it('should work without min and max', () => expect(axisDeadZone(0.525)).toBeCloseTo(0.5, 5));
 
 });
