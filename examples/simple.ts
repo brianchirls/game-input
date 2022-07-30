679c488fa6d200ff6ff247b9702cfd8e68997dcc
Brian Chirls <brian@chirls.com>
2022-07-30 10:29:11 -0400

    Clean up simple example

--- examples/simple.ts
+++ examples/simple.ts
@@ -1,34 +1,38 @@
+import html from 'html-loader!./simple.html';
 import Gamepad from '../src/devices/gamepad';
 import Keyboard from '../src/devices/keyboard';
-import Pointer from '../src/devices/pointer';
 import DPadComposite from '../src/controls/DPadComposite';
 import AxisComposite from '../src/controls/AxisComposite';
 import Action from '../src/Action';
-import PressInteraction from '../src/interactions/PressInteraction';
-import ReleaseInteraction from '../src/interactions/ReleaseInteraction';
 import VirtualStick from '../src/devices/virtualstick';
 import domView from '../src/devices/virtualstick/domView';
-import { StickInputControl } from '../src';
 
-// Devices
+document.body.innerHTML = html;
+
+/**
+ * Devices
+ */
 const gamepad = new Gamepad();
 const kbd = new Keyboard({
+	/*
+	`keyCode` option makes this work on different layouts so WASD becomes
+	ZQSD on "AZERTY" (e.g., French) keyboards.
+	*/
 	keyCode: true
 });
-const pointer = new Pointer({
-	touch: false,
-	enabled: false
-});
 const leftTouch = new VirtualStick({
+	// constrain to left side of screen
 	filter: evt => evt.pageX < Math.max(200, window.innerWidth * 0.4)
 });
 const rightTouch = new VirtualStick({
+	// constrain to right side of screen
 	filter: evt => evt.pageX > window.innerWidth - Math.max(200, window.innerWidth * 0.4)
 });
 
-// Controls
-const leftStick = gamepad.getControl('leftStick') as StickInputControl;
-// const downButton = leftStick.find('down');
+/**
+ * Controls
+ */
+const leftStick = gamepad.getControl('leftStick');
 const rightStickHoriz = gamepad.getControl('rightStick').x;
 
 const kbdWASD = new DPadComposite({
@@ -46,63 +50,39 @@ const rotateArrowKeys = new AxisComposite({
 const leftTouchStick = leftTouch.getControl();
 const rightTouchStick = rightTouch.getControl();
 
-// Actions
+/**
+ * Actions
+ */
 const moveAction = new Action<[number, number]>({
 	bindings: [
 		leftStick,
 		kbdWASD,
-		leftTouchStick,
-		{
-			control: pointer.getControl('delta'),
-			processors: [
-				(vec: [number, number]) => [
-					vec[0] / 8,
-					-vec[1] / 8
-				] as [number, number]
-			]
-		}
+		leftTouchStick
 	]
 });
+
 const rotateAction = new Action<number>({
 	bindings: [
 		rightStickHoriz,
 		rotateArrowKeys,
-		rightTouchStick.find('x'),
-		pointer.getControl('wheel')
+		rightTouchStick.x
 	]
 });
 
-const buttonAction = new Action({
-	bindings: [kbd.getControl(' ')]
-});
-
-const pressButton = new PressInteraction(buttonAction);
-pressButton.on('*', evtName => console.log('press interaction', evtName, pressButton.state));
-
-const releaseButton = new ReleaseInteraction(buttonAction);
-releaseButton.on('*', evtName => console.log('release interaction', evtName, releaseButton.state));
-
-const thing = document.createElement('div');
-Object.assign(thing.style, {
-	position: 'absolute',
-	width: '30px',
-	height: '60px',
-	backgroundColor: 'rebeccapurple',
-	margin: '-15px 30px'
-});
-document.body.appendChild(thing);
+const thing = document.getElementById('thing') as HTMLElement;
 
 const speed = 300 / 1000; // pixels per 1000ms
-const rotSpeed = 360 / 1000; // degrees per 1000ms
+const rotSpeed = 420 / 1000; // degrees per 1000ms
 let x = 100;
 let y = 100;
-let r = 30;
+let r = 0;
 
 let last = performance.now();
 function update(t = performance.now()) {
 	const delta = t - last;
 	last = t;
 
+	// gamepad is a polling device and won't work unless we update on every frame
 	gamepad.update();
 
 	const [dx, dy] = moveAction.value;
@@ -119,59 +99,8 @@ function update(t = performance.now()) {
 	requestAnimationFrame(update);
 }
 
+// visualize virtual sticks
 domView(leftTouchStick);
 domView(rightTouchStick);
 
 update();
-
-// Toggle devices
-const devices = [
-	['Gamepad', gamepad],
-	['Keyboard', kbd],
-	['Pointer', pointer]
-] as [string, { enabled: boolean; }][];
-
-if (leftTouch.connected) {
-	devices.push([
-		'Virtual Stick',
-		{
-			get enabled() {
-				return leftTouch.enabled;
-			},
-			set enabled(val) {
-				leftTouch.enabled = val;
-				rightTouch.enabled = val;
-			}
-		}
-	]);
-}
-
-const controls = document.createElement('div');
-Object.assign(controls.style, {
-	position: 'fixed',
-	top: '20px',
-	right: '20px',
-	border: 'white solid 1px',
-	backgroundColor: 'rgba(0, 0, 0, 0.6)',
-	color: '#ffffff',
-	display: 'flex',
-	flexDirection: 'column',
-	padding: '20px'
-});
-document.body.appendChild(controls);
-devices.forEach(([name, device]) => {
-	const label = document.createElement('label');
-	const input = document.createElement('input');
-	Object.assign(input, {
-		type: 'checkbox',
-		checked: device.enabled,
-		onclick() {
-			device.enabled = !device.enabled;
-		}
-	});
-
-	label.appendChild(input);
-	label.appendChild(document.createTextNode(' ' + name));
-
-	controls.appendChild(label);
-});
